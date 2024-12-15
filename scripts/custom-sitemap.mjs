ace0ebab7a4945955b9dde8aa7ecb65acd002583
Sohail Jafri <66732877+thesohailjafri@users.noreply.github.com>
2024-12-15 12:31:49 +0530

    Update custom-sitemap.mjs

--- scripts/custom-sitemap.mjs
+++ scripts/custom-sitemap.mjs
@@ -65,12 +65,12 @@ async function generateSitemap() {
             changefreq = 'daily'
           }
           return `<url>
-								<loc>${loc}</loc>
-								<lastmod>${lastmod}</lastmod>
-								<changefreq>${changefreq}</changefreq>
-								<priority>${priority}</priority>
-						</url>
-					`
+	  	<loc>${loc}</loc>
+		<lastmod>${lastmod}</lastmod>
+		<changefreq>${changefreq}</changefreq>
+		<priority>${priority}</priority>
+		</url>
+		`
         })
         .join('')}
 	</urlset>
